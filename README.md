c860b62dafcec1a319460c1134bb96c891682a99
Derek Haynes <derek.haynes@gmail.com>
2016-05-10 10:24:52 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rails 5.0 Testing App for ScoutAPM
+
+This is a test app to ensure ScoutAPM is compatible with Rails 5.0.X and Ruby 2.3.
+
+## Bootstrap
+
+* bundle
+* rake db:create db:migrate db:seed
+
+## Starting the app
+
+SCOUT_KEY=YOUR_KEY rails server -p8080
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  def index
+    do_stuff
+  end
+
+  # Generate a bunch of fake endpoints. These can reached like: http://127.0.0.1:8080/users/index1/id.
+  # Used to add more metric diversity to reporting periods.
+  (1..100).to_a.each do |i|
+    send :define_method, "index#{i}" do
+      do_stuff
+    end
+  end
+
+  ### Benchmarking-utility actions...not actually hit during benchmarks ###
+
+  def ip
+    render text: local_ip
+  end
+
+  # Generate a file of URLs for load testing.
+  def urls
+    @ip = local_ip
+    render 'urls.plain.erb', layout: false, content_type: 'text/plain'
+  end
+
+  private
+
+  def local_ip
+    orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true  # turn off reverse DNS resolution temporarily
+
+    UDPSocket.open do |s|
+      s.connect '64.233.187.99', 1
+      s.addr.last
+    end
+  ensure
+    Socket.do_not_reverse_lookup = orig
+  end
+
+  def do_stuff
+    # used to add slow requests to an app and force a higher level of data collection in APM agent.
+    sleep 2# if rand(100000) <= 434 # 0.43%
+    @users = User.limit(1000).all
+    render action: 'index'
+  end
+end
